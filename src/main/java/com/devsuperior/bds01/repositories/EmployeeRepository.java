54cefa90a38c6c0c5b1689ebbde44528b76b749f
jefferson barbosa dos santos <jefferson.bds@hotmail.com>
2023-02-09 21:19:09 -0300

    Tests passing

---
+++ src/main/java/com/devsuperior/bds01/repositories/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.devsuperior.bds01.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.bds01.entities.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+	
+}
